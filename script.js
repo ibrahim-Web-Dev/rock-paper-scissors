96f27eb01f304fb900f76e8a895f24b93f0ace6a
ibrahim-Web-Dev <ibrahimyilmaz0713@gmail.com>
2024-10-02 22:19:51 +0300

    finished project

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-let humanScore=0;
-let computerScore=0;
+
 function getComputerChoice(){
     switch (Math.floor(Math.random()*3))
     {
@@ -26,6 +25,73 @@ function getHumanChoice(){
             return getHumanChoice()
     }
 }
-
-console.log(getHumanChoice())
-console.log(getComputerChoice())
+function playGame(){
+    let humanScore=0;
+    let computerScore=0;
+    function playRound(humanChoice, computerChoice){
+        console.log("Human choice is " + humanChoice + " computer choice is "+ computerChoice)
+        switch (humanChoice){
+        case "rock":
+            switch (computerChoice){
+                case "rock":
+                    console.log("draw")
+                    break
+                case "paper":
+                    console.log("you lose")
+                    computerScore++
+                    break
+                case "scissor":
+                    console.log("you win")
+                    humanScore++
+                    break
+        }
+            break;
+        case "paper":
+            switch (computerChoice){
+                case "rock":
+                    console.log("you win")
+                    humanScore++
+                    break;
+                case "paper":
+                    console.log("draw")
+                    break;
+                case "scissor":
+                    console.log("you lose")
+                    computerScore++
+                    break;
+        }
+            break;
+        case "scissor":
+            switch (computerChoice){
+            case "rock":
+                console.log("you lose")
+                computerScore++
+                break;
+            case "paper":
+                console.log("you win")
+                humanScore++
+                break;
+            case "scissor":
+                console.log("draw")
+                break;
+            }
+        }
+    console.log("your score is " +humanScore+ " computer score is " + computerScore + " good match.")
+    console.log("")
+    }
+    playRound(getHumanChoice(),getComputerChoice())
+    playRound(getHumanChoice(),getComputerChoice())
+    playRound(getHumanChoice(),getComputerChoice())
+    playRound(getHumanChoice(),getComputerChoice())
+    playRound(getHumanChoice(),getComputerChoice())
+    if (humanScore>computerScore){
+        console.log("You win the score is " +humanScore+ " - "+computerScore)
+    }
+    else if (computerScore>humanScore){
+        console.log("You lose  the score is " +humanScore+ " - "+computerScore)
+    }
+    else{
+        console.log("Draw the score is " +humanScore+ " - "+computerScore)
+    }
+}
+playGame()
